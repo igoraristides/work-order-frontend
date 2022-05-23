f2d407fb4f65167bbfbbcb5e6ebbe098d7f6a138
Natã <nata.rcj@pti.org.br>
2022-05-23 20:17:22 -0300

    Adicionado mascaras ao cadastro de cliente

--- src/pages/UserRegistration/UserRegistrationForm.tsx
+++ src/pages/UserRegistration/UserRegistrationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box, Button } from "@mui/material";
 import { ControlledTextField } from "../../components/HookForm/TextField/TextField";
 import useHookForm from "../../hooks/UseHookForm/useHookForm";
@@ -7,10 +7,9 @@ import { userRegistrationSchema } from "./UserRegistration.schema";
 import Form from "../../components/HookForm/Form/Form";
 import { NewClient } from "../../api/api";
 import { toast } from "react-toastify";
-
+import FormUtils from "../../components/HookForm/formUtils/FormUtils";
 
 const UserRegistrationForm: React.FC<any> = () => {
-
   const initialValues: UserRegistration = {
     firstName: "",
     lastName: "",
@@ -20,77 +19,84 @@ const UserRegistrationForm: React.FC<any> = () => {
     email: "",
   };
 
-  const FORM_ID = 'NewUser'
+  const FORM_ID = "NewUser";
 
-  const { methods } = useHookForm(initialValues, userRegistrationSchema(), 'onSubmit')
+  const { methods } = useHookForm(
+    initialValues,
+    userRegistrationSchema(),
+    "onSubmit"
+  );
 
   const HandleSubmit = async (formValues: any) => {
-
     try {
       const response = await NewClient(formValues);
       toast.success("Usuário cadastrado com sucesso");
-      methods.reset()
+      methods.reset();
     } catch {
       toast.error("Falha ao cadastrar usuário");
-      methods.reset()
+      methods.reset();
     }
-
-  }
-
-
+  };
 
   return (
-
-    <Box style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+    <Box style={{ display: "flex", flexDirection: "column", width: "100%" }}>
       <Form id={FORM_ID} methods={methods} onSubmit={HandleSubmit}>
-        <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+        <div
+          style={{ display: "flex", flexDirection: "column", width: "100%" }}
+        >
           <ControlledTextField
-            name='firstName'
+            name="firstName"
             control={methods.control}
-            label='Primeiro Nome'
-            style={{ marginBottom: '10px' }}
+            label="Primeiro Nome"
+            style={{ marginBottom: "10px" }}
           />
           <ControlledTextField
-            name='lastName'
+            name="lastName"
             control={methods.control}
-            label='Último Nome'
-            style={{ marginBottom: '10px' }}
+            label="Último Nome"
+            style={{ marginBottom: "10px" }}
           />
           <ControlledTextField
-            name='cpf'
+            name="cpf"
             control={methods.control}
-            label='CPF'
-            style={{ marginBottom: '10px' }}
+            label="CPF"
+            style={{ marginBottom: "10px" }}
+            mask={FormUtils.cpfMask}
           />
           <ControlledTextField
-            name='telephone'
+            name="telephone"
             control={methods.control}
-            label='Telefone'
-            style={{ marginBottom: '10px' }}
-            mask={['(', /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/]}
+            label="Telefone"
+            style={{ marginBottom: "10px" }}
+            mask={FormUtils.phoneMask}
           />
           <ControlledTextField
-            name='cellphone'
+            name="cellphone"
             control={methods.control}
-            label='Celular'
-            style={{ marginBottom: '10px' }}
-            mask={['(', /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/]}
+            label="Celular"
+            style={{ marginBottom: "10px" }}
+            mask={FormUtils.cellPhoneMask}
           />
           <ControlledTextField
-            name='email'
+            name="email"
             control={methods.control}
-            label='E-mail'
-            style={{ marginBottom: '10px' }}
+            label="E-mail"
+            style={{ marginBottom: "10px" }}
           />
         </div>
       </Form>
       <Box textAlign="center">
-        <Button variant="contained" type="submit" form={FORM_ID} color={"primary"} style={{ marginTop: '20px' }}>
+        <Button
+          variant="contained"
+          type="submit"
+          form={FORM_ID}
+          color={"primary"}
+          style={{ marginTop: "20px" }}
+        >
           Cadastrar
         </Button>
       </Box>
     </Box>
-
   );
 };
 
